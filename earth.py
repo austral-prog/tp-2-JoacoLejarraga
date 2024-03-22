418afa5ea881180c0722ffcc29a7c8e18262365d
JoacoLejarraga <162721835+JoacoLejarraga@users.noreply.github.com>
2024-03-22 12:39:04 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -7,4 +7,3 @@ def earth():
     print (f"The result of Bangladesh comes first in the dictionary than Barbados is {a}.")
 
     print (f"The result of Barbados comes first in the dictionary than Bangladesh is {b}.")
-earth()
